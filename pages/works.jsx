b0e8f30615f8d511e7473a3db2e52e564e0891b1
phamminhhieu85 <minhhieupham851999@gmail.com>
2022-07-15 03:03:46 +0700

    add 3d for work item and change cursor img

--- pages/works.jsx
+++ pages/works.jsx
@@ -2,8 +2,10 @@ import React, { useCallback, useEffect, useState, useRef } from "react";
 import { motion, useAnimation } from "framer-motion";
 import { useInView } from "react-intersection-observer";
 import useEmblaCarousel from "embla-carousel-react";
+import Image from "next/image";
 
 const Item = ({ item }) => {
+  const itemRef = useRef();
   //animate
   const controls = useAnimation();
   const [ref, inView] = useInView();
@@ -11,30 +13,55 @@ const Item = ({ item }) => {
   useEffect(() => {
     if (inView) {
       controls.start({
-        y: 0,
-        opacity: 1,
+        scale: 1,
+        // opacity: 1,
         transition: {
-          duration: 2,
+          duration: 1,
           type: "spring",
         },
       });
     } else {
       controls.start({
-        y: 100,
-        opacity: 0,
+        scale: 0,
+        // opacity: 0,
       });
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [inView, controls]);
 
+  const [calc, setCalc] = useState([0, 0]);
+
+  const handleMouseMove = useCallback((e) => {
+    let box = itemRef.current.getBoundingClientRect();
+    let calcX = (e.clientY - box.y - box.height / 2) / 20;
+    let calcY = (e.clientX - box.x - box.width / 2) / 20;
+
+    setCalc([calcX, calcY]);
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, [handleMouseMove]);
+
   return (
-    <div className="relative w-full h-full ">
+    <div className="relative w-full h-full" onMouseMove={handleMouseMove}>
+      {/* {console.log(mouse)} */}
       <motion.div
         ref={ref}
         animate={controls}
         className="w-full h-full flex justify-center items-center snap-center"
       >
-        <div className="h-[200px] sm:h-[300px] lg:h-[500px] w-[80%] md:w-[60%] bg-orange-50 opacity-70 flex justify-center items-center">
+        <div
+          ref={itemRef}
+          className="relative work-item h-[200px] sm:h-[300px] lg:h-[500px] w-[80%] md:w-[60%] bg-orange-50 flex justify-center items-center"
+          style={{
+            transform: `rotateX(${calc[0]}deg) rotateY(${calc[1]}deg)`,
+          }}
+        >
           <h1 className="text-3xl text-red-400">Section {item}</h1>
         </div>
       </motion.div>
@@ -45,14 +72,12 @@ const Item = ({ item }) => {
 const Carousel = () => {
   const scrollRef = useRef();
 
-  const [viewportRef, embla] = useEmblaCarousel(
-    {
-      axis: "y",
-      skipSnaps: false,
-      loop: true,
-      align: "end",
-    }
-  );
+  const [viewportRef, embla] = useEmblaCarousel({
+    axis: "y",
+    skipSnaps: false,
+    loop: true,
+    align: "end",
+  });
 
   const handleScroll = useCallback(
     (e) => {
